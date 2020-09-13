a0b047665081c165f08553947c633666ecaa727c
Ilya Tolokonov <itolokonov@mail.ru>
2020-09-13 16:29:51 +0300

    enter

--- com/company/pr3_task_4_5/Test.java
+++ com/company/pr3_task_4_5/Test.java
@@ -3,6 +3,7 @@ package com.company.pr3_task_4_5;
 public class Test {
     public static void main(String[] args) {
         MovableCircle point = new MovableCircle(5, 5, 1, 1, 2);
+
         System.out.println(point);
         System.out.println();
         point.moveDown();
@@ -10,6 +11,7 @@ public class Test {
         System.out.println();
 
         MovableRectangle rectangle = new MovableRectangle(6, 7, 8, 5, 1, 1);
+
         System.out.println(point);
         System.out.println();
         point.moveDown();

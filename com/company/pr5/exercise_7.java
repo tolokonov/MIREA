e1d3c225c5d760f3f38ec2dd77d4447bf5e7794a
Tolokonov Ilya <itolokonov@mail.ru>
2020-09-21 11:26:15 +0300

    Create exercise_7.java

---
+++ com/company/pr5/exercise_7.java
@@ -0,0 +1,17 @@
+package com.company;
+
+class exercise_7 {
+    public static void recursion (int n, int k){
+        if (k > n / 2) {
+            System.out.println(n);
+            return;
+        }
+
+        if (n % k == 0) {
+            System.out.println(k);
+            recursion(n / k, k);
+        } else {
+            recursion(n, ++k);
+        }
+    }
+}

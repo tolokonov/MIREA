3e6736366706d238aae7251f83073307236b2e9d
Tolokonov Ilya <itolokonov@mail.ru>
2020-09-21 11:31:14 +0300

    Create exercise_11.java

---
+++ com/company/pr5/exercise_11.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class exercise_11 {
+
+    public static int recursion() {
+        Scanner scanner = new Scanner(System.in);
+
+        int i = 0;
+        int n = scanner.nextInt();
+
+        if (n == 1) {
+            i++;
+            int m = scanner.nextInt();
+
+            if (m == 1) {
+                i++;
+                return recursion() + i;
+            } else if (m == 0){
+                int k = scanner.nextInt();
+                if (k == 1) {
+                    i++;
+                    return recursion() + i;
+                } else if (k==0){
+                    return i;
+                } else {
+                    return recursion() + i;
+                }
+            } else {
+                return recursion() + i;
+            }
+        } else if (n==0){
+            int m = scanner.nextInt();
+
+            if (m == 1) {
+                i++;
+                return recursion() + i;
+            } else if (m == 0){
+                return i;
+            } else {
+                return recursion() + i;
+            }
+        } else {
+            return recursion() + i;
+        }
+    }
+}

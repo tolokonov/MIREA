4a0f7d4e40350300672d4ba9359a4eb31740da3c
Tolokonov Ilya <itolokonov@mail.ru>
2020-10-12 18:56:15 +0300

    Create Queue_.java

---
+++ com/company/pr7/Queue_.java
@@ -0,0 +1,47 @@
+package com.company.pr7;
+
+import java.util.Queue;
+
+public class Queue_ {
+    public static String play(Queue cards_1, Queue cards_2){
+
+        for (int i=0; i<106; i++){
+            if (cards_1.size() == 0){
+                System.out.println("Количество шагов равно: " + i);
+                return "second";
+            } else if (cards_2.size() == 0){
+                System.out.println("Количество шагов равно: " + i);
+                return "first";
+            } else {
+                Integer card_1 = (int) cards_1.peek();
+                Integer card_2 = (int) cards_2.peek();
+                if (card_1 != 0 && card_2!=0){
+                    if (card_1 > card_2){
+                        cards_1.remove();
+                        cards_2.remove();
+                        cards_1.add(card_1);
+                        cards_1.add(card_2);
+                    } else if (card_1 < card_2){
+                        cards_1.remove();
+                        cards_2.remove();
+                        cards_2.add(card_2);
+                        cards_2.add(card_1);
+                    }
+                } else if(card_1 == 9 || card_2==9){
+                    if (card_1 == 0){
+                        cards_1.remove();
+                        cards_2.remove();
+                        cards_1.add(card_1);
+                        cards_1.add(card_2);
+                    } else if (card_2 == 0){
+                        cards_1.remove();
+                        cards_2.remove();
+                        cards_2.add(card_2);
+                        cards_2.add(card_1);
+                    }
+                }
+            }
+        }
+        return "bolda";
+    }
+}

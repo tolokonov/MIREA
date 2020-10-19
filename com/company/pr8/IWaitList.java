b2409decaa1730202c1572c5faaa9c3dc6cf1722
Tolokonov Ilya <itolokonov@mail.ru>
2020-10-19 10:52:32 +0300

    Update IWaitList.java

--- com/company/pr8/IWaitList.java
+++ com/company/pr8/IWaitList.java
@@ -1,5 +1,23 @@
 package com.company.pr8;
 
+/** Many methods in Collections Framework interfaces are defined in
+ * terms of the {@link Object#equals(Object) equals} method.  For example,
+ * the specification for the {@link #contains(Object) contains(Object o)}
+ * method says: "returns {@code true} if and only if this collection
+ * contains at least one element {@code e} such that
+ * {@code (o==null ? e==null : o.equals(e))}."  This specification should
+ * <i>not</i> be construed to imply that invoking {@code Collection.contains}
+ * with a non-null argument {@code o} will cause {@code o.equals(e)} to be
+ * invoked for any element {@code e}.  Implementations are free to implement
+ * optimizations whereby the {@code equals} invocation is avoided, for
+ * example, by first comparing the hash codes of the two elements.  (The
+ * {@link Object#hashCode()} specification guarantees that two objects with
+ * unequal hash codes cannot be equal.)  More generally, implementations of
+ * the various Collections Framework interfaces are free to take advantage of
+ * the specified behavior of underlying {@link Object} methods wherever the
+ * implementor deems it appropriate.
+*/
+
 import java.util.Collection;
 
 public interface IWaitList<T> {

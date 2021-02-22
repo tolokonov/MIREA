ba0a6e83e55b94f6260dfe3db95c9a686d5f5ff6
Tolokonov Ilya <itolokonov@mail.ru>
2021-02-22 22:37:20 +0300

    Update 2.py

--- python/2.py
+++ python/2.py
@@ -49,4 +49,43 @@ def f22(x):
     b = q[3:15]
     a = q[15:31]
     h = c + a + b  
-  return hex(int(h, 2))    
+  return hex(int(h, 2))
+
+def f23(a):
+  new_a = []
+  [new_a.append(item) for item in a if item not in new_a]
+
+  new_b = []
+  new_c = []
+  for item in new_a:
+    for item_1 in item:
+      if item_1 != 'None':
+        new_b.append(item_1)
+    new_c.append(new_b)
+    new_b = []    
+  
+  for i in range(len(new_c[0])):
+    new_a = []
+    for item in new_c:
+      new_a.append(item[i])
+    new_b.append(new_a)
+  new_a = []
+  [new_a.append(item) for item in new_b if item not in new_a]
+
+  for i in range(len(new_a[0])):
+    if new_a[0][i] == 'Y':
+      new_a[0][i] = 'true'
+    else:
+      new_a[0][i] = 'false'
+
+  for i in range(len(new_a[1])):
+    new_a[1][i] = new_a[1][i].replace('[at]', '@')
+
+  for i in range(len(new_a[2])):
+    new_a[2][i] = new_a[2][i][4:7] + new_a[2][i][8:]
+
+  for i in range(len(new_a[3])):
+    new_a[3][i] = new_a[3][i].replace('%', '')
+    temp = float(new_a[3][i]) / 100.0
+    new_a[3][i] = str(round(temp, 1))
+  return new_a
